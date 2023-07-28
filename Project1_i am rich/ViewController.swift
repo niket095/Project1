8c0f575812f133c45690fad10647353fa29da44f
niket095 <95niket.putilov@gmail.com>
2023-07-28 16:27:59 +0500

    Project1

---
+++ Project1_i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1_i am rich
+//
+//  Created by User on 25.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
